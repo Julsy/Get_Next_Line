690f8ec637169d49a35f33fb3e39e05396d90161
Julia Iliuk <julia.iluyk@gmail.com>
2016-12-06 11:32:03 -0800

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -15,18 +15,18 @@
 # include "libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
-# define BUFF_SIZE 32
+# define BUFF_SIZE 42
 
 typedef struct		s_lst
 {
 	void			*left;
 	size_t			left_size;
-	struct s_lst	*next;
-	int				fd;
-}					t_lst;
+	struct s_lst		*next;
+	int			fd;
+}			t_lst;
 
-int					get_next_line(int const fd, char **line);
-int					check_extra(int fd, char *buf, t_lst **head);
+int				get_next_line(int const fd, char **line);
+int				check_extra(int fd, char *buf, t_lst **head);
 t_lst				*find_extra(int fd, t_lst **head);
 char				*get_extra(int fd, t_lst **head);
 
